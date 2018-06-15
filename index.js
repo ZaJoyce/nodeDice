68c584ba55a14abc9844b10dabc3ff159262e70a
Zach Joyce <zjoyce@FusionAdmins-MacBook-Pro-5.local>
2018-06-15 14:26:23 -0400

    adding terminal argument capabilities

--- index.js
+++ index.js
@@ -2,4 +2,14 @@
 
 const roll = require('./app/roll')
 
-console.log(roll(3,6));
+let diceString = process.argv[2];
+
+let diceArray = diceString.split('d')
+
+let count = diceArray[0];
+let sides = diceArray[1];
+
+
+console.log(diceArray);
+
+console.log(roll(count, sides));
