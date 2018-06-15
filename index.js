c662f4a5b46a44a99f0084ea6d6f02a90300c42c
Zach Joyce <zjoyce@FusionAdmins-MacBook-Pro-5.local>
2018-06-15 09:18:24 -0400

    added for loop roll.js as well

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
 const getRandomInt = require('./app/random');
+let rolltotal = 0
+let rolls = [];
+
+for(let i=0; i<100; i++){
+    
+    rolls.push(getRandomInt(1,6))
+    
+    rolltotal += rolls[i]
+    
+
+}
+console.log(rolltotal/100)
 
 console.log(getRandomInt(1,6));
