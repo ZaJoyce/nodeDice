340052f119e687e393fba135b19ca5212e9f20c3
Zach Joyce <zjoyce@FusionAdmins-MacBook-Pro-5.local>
2018-06-15 21:19:43 -0400

    adding console.table

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ let diceArray = diceString.split('d')
 let count = diceArray[0];
 let sides = diceArray[1];
 
+
+
 let results = app.roll(count, sides);
 let sum = app.total(results);
 
 
-console.log(results + ' ' + sum);
+console.table(results);
+console.log(sum);
 
 // todo: add double checking function
